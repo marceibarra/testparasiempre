467bdb762f6eb338a6d2afce02d404d85b70c4ab
marceloibarra <marceibarra@users.noreply.github.com>
2018-01-31 14:28:48 -0300

    -

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Sua mensagem foi enviada. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -44,7 +44,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Desculpas, " + firstName + ", o servidor não responde. Intente novamente."));
+          $('#success > .alert-danger').append($("<strong>").text("Desculpas " + firstName + ", o servidor não responde. Intente novamente."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
