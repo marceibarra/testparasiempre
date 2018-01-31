2da5b860a19afbc04507a4b5e3ac93db91c8a5c3
marceloibarra <marceibarra@users.noreply.github.com>
2018-01-31 15:49:50 -0300

    -

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'maria.marketing@romapy.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Você recebeu um email do site da Next Fun\n\n"."Estes são os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nMensagem:\n$message";
-$headers = "From: noreply@romapy.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: maria.marketing@romapy.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
